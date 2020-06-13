ee6c545cfd929e855cbf6ee9ea2a71692b94f990
Javier Moreno <morehendrix@gmail.com>
2020-06-13 17:29:15 +0000

    Comentarios e interacción en español

--- 1-Caesar/Caesar.py
+++ 1-Caesar/Caesar.py
@@ -1,21 +1,20 @@
-plainText = input("Introduce text to cipher: ")
-shift = int(input("Introduce shift: "))
+plainText = input("Introduce el texto a cifrar: ")
+shift = int(input("Introduce el desplazamiento: "))
 
 def encrypt(plainText,shift):
   result = ""
   for i in range(len(plainText)):
     char = plainText[i]
-     
-    # Encrypt uppercase characters in plain text
+    # Mayúsculas - A = ASCII(65)
     if (char.isupper()):
       result += chr((ord(char) + shift-65) % 26 + 65)
-    # Encrypt lowercase characters in plain text
+    # Minúsculas - a = ASCII(97)
     elif (char.islower()):
       result += chr((ord(char) + shift - 97) % 26 + 97)
+    # El resto de caracteres los dejamos igual
     else:
       result += char
   return result
-  #check the above function
 
 cipherText = encrypt(plainText, shift)
-print("Ciphertext is: ", cipherText)
+print("El texto cifrado es: ", cipherText)
