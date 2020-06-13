a3079db03d49418e581c2a246eef7405b1e0acbe
Javier Moreno Molina <javier.moreno_molina@daimler.com>
2020-06-13 18:15:42 +0200

    Added Caesar example

---
+++ 1-Caesar/Caesar.py
@@ -0,0 +1,21 @@
+plainText = raw_input("Introduce text to cipher: ")
+shift = int(raw_input("Introduce shift: "))
+
+def encrypt(plainText,shift):
+  result = ""
+  for i in range(len(plainText)):
+    char = plainText[i]
+     
+    # Encrypt uppercase characters in plain text
+    if (char.isupper()):
+      result += chr((ord(char) + shift-65) % 26 + 65)
+    # Encrypt lowercase characters in plain text
+    elif (char.islower()):
+      result += chr((ord(char) + shift - 97) % 26 + 97)
+    else:
+      result += char
+  return result
+  #check the above function
+
+cipherText = encrypt(plainText, shift)
+print "Ciphertext is: ", cipherText
